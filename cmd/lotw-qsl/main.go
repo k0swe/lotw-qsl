de2c1a095409ec67ffa07e6f193622f1c48ba249
Chris Keller <xylo04@gmail.com>
2020-11-02 15:15:32 -0700

    Add start date to driver

--- cmd/lotw-qsl/main.go
+++ cmd/lotw-qsl/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/antihax/optional"
 	"github.com/k0swe/lotw-qsl"
 	"os"
 )
@@ -10,13 +11,18 @@ import (
 func main() {
 	user := flag.String("username", "", "QRZ.com login name")
 	pw := flag.String("password", "", "QRZ.com password")
+	since := flag.String("since", "", "Beginning date to query for; YYYYMMDD format")
 	flag.Parse()
 	if *user == "" || *pw == "" {
 		flag.PrintDefaults()
 		os.Exit(1)
 	}
 
-	adifStr, err := lotw.Query(*user, *pw, &lotw.QueryOpts{})
+	opts := &lotw.QueryOpts{}
+	if *since != "" {
+		opts.QsoStartdate = optional.NewString(*since)
+	}
+	adifStr, err := lotw.Query(*user, *pw, opts)
 	if err != nil {
 		_, _ = fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
