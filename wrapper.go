6bf95b6133423dc945fe8ab2c5ed275a4beaaf9c
Jason Hancock <jason@jasonhancock.com>
2020-12-12 14:54:14 -0800

    Add context-aware query function. (#8)
    I've noticed that sometimes the LotW API takes quite a while to respond
    and being able to time out or cancel the queries by using a context
    would be nice.

--- wrapper.go
+++ wrapper.go
@@ -8,14 +8,18 @@ import (
 	"context"
 )
 
-/*
-Query LotW and return an ADIF string with the results.
-*/
+// Query LotW and return an ADIF string with the results.
 func Query(user string, pw string, opts *QueryOpts) (string, error) {
+	return QueryContext(context.Background(), user, pw, opts)
+}
+
+// QueryContext performs a context aware LotW query and returns an ADIF string
+// with the results.
+func QueryContext(ctx context.Context, user string, pw string, opts *QueryOpts) (string, error) {
 	config := NewConfiguration()
 	config.UserAgent = "k0swe/lotw-qsl 0.0.1"
 	client := NewAPIClient(config)
-	queryResp, _, err := client.DefaultApi.Query(context.TODO(), user, pw, 1, opts)
+	queryResp, _, err := client.DefaultApi.Query(ctx, user, pw, 1, opts)
 	if err != nil {
 		return "", err
 	}
